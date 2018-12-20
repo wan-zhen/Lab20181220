81ed84b392174f03f7c55c2f24978c9bfca4a365
wilson <l7960261@gmail.com>
2018-12-20 10:36:18 +0800

    add VUE_APP_API_ENDPOINT_HOT in .env

--- src/services/BannerService.js
+++ src/services/BannerService.js
@@ -2,7 +2,7 @@ import Api from './Api';
 
 export default {
   getHot() {
-    return Api().get('/api/json/get/4JUPPpc1L');
+    return Api().get(`/api/json/get/${process.env.VUE_APP_API_ENDPOINT_HOT}`);
   },
   getNews() {
     return Api().get('/api/json/get/NynTtqEAr');
